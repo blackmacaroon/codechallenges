3ba279ab96b2e9803076583e12b4767c3a737ebb
Kayla <6lackmacaroon@gmail.com>
2020-04-29 19:28:00 -0600

    commented for clarity

--- jumpgame.js
+++ jumpgame.js
@@ -10,14 +10,20 @@ arr1 = [ 3, 2, 1, 0, 4 ]
 arr2 = [ 0, 2, 3, 1 ]
 
 var canJump = function(nums) {
+    // edge cases? non negative, array length? 
+    // always starts at array 1
+    if(nums.length === 1) return true;
+    // start at the end of the array
     let lastGoodIndex = nums.length-1
+    // traverse backwards
+    // if you're at the last index, check if each value at the prior index is good to get to the last one, yes/no
     for (let i=nums.length-1; i >= 0; i--){
-    // non negative
-    // always start at array 1
+        // if the current position + nums[i] is >= to the lastGoodIndex, then swap that value for the last good index position
         if(i + nums[i] >= lastGoodIndex ){
             lastGoodIndex = i
         }
     }
+    // return boolean
     return lastGoodIndex == 0
 };
 
